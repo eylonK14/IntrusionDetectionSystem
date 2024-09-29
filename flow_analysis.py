cd1dfe12e8eec35242efdc7ba1792016dfb471d7
eylonK14 <eylony1403@gmail.com>
2024-09-29 21:20:43 +0300

    started flow analysis

---
+++ flow_analysis.py
@@ -0,0 +1,23 @@
+import sys
+from scapy.all import IP, TCP, UDP
+
+
+def create_five_tuple(pkt):
+    print(pkt.payload.layers()[0])
+    if pkt.haslayer(IP) and pkt.haslayer(pkt.payload.layers()[1]):
+        return (pkt[IP].src, pkt[pkt.payload.layers()[1]].sport, pkt[IP].dst, pkt[pkt.payload.layers()[1]].dport, pkt.payload.layers()[1].__name__)
+    if (not pkt.haslayer(TCP) and not pkt.haslayer(UDP)):
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(pkt.payload.layers())
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        sys.exit(1)
+    
+    #if TCP in pkt:
+    #    print(pkt.payload.layers())
+    #    return (pkt[IP].src, pkt[TCP].sport, pkt[IP].dst, pkt[TCP].dport, TCP)
+    #elif UDP in pkt:
+    #    print(pkt.payload.layers()[2])
+     #   return (pkt[IP].src, pkt[UDP].sport, pkt[IP].dst, pkt[UDP].dport, pkt[UDP].sport)
+
+
+
